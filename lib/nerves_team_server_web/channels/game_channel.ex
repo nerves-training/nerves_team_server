5e5fe93644f830deec88b94fbc32e0bafa5dce5a
Justin Schneck <jschneck@mac.com>
2019-02-01 06:15:43 -0800

    step16

--- lib/nerves_team_server_web/channels/game_channel.ex
+++ lib/nerves_team_server_web/channels/game_channel.ex
@@ -1,7 +1,7 @@
 defmodule NervesTeamServerWeb.GameChannel do
   use NervesTeamServerWeb, :channel
 
-  alias NervesTeamGame.Lobby
+  alias NervesTeamGame.{Lobby, Game}
 
   def join("game:lobby", payload, socket) do
     if authorized?(payload) do
@@ -12,11 +12,26 @@ defmodule NervesTeamServerWeb.GameChannel do
     end
   end
 
+  def join("game:" <> game_id,
+    %{"player_id" => player_id}, socket) do
+
+    {:ok, player} = Game.player_join(game_id, player_id)
+    {:ok, assign(socket, :player, player)}
+  end
+
   def handle_in("player:ready", %{"ready" => ready?}, socket) do
     {:ok, _player} =
       Lobby.ready_player(socket.assigns.player.id, ready?)
     {:reply, :ok, socket}
   end
+
+  def handle_in("action:execute", action,
+    %{topic: "game:" <> game_id} = socket) do
+
+    Game.action_execute(game_id, action)
+    {:reply, :ok, socket}
+  end
+
 
   # Pass messages from game server to the client
   def handle_info({event, payload}, socket) do
